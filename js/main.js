f4d24303847a07130805e0e8f8e411a63835543a
Nero978 <zhumengfeiyang@qq.com>
2023-08-03 02:38:37 +0800

    优化版本号逻辑

--- js/main.js
+++ js/main.js
@@ -8,26 +8,3 @@ addEventListener('scroll', () => {
     }
 });
 
-
-document.addEventListener("DOMContentLoaded", function () {
-    getVersion();
-});
-
-function getVersion() {
-    fetch('https://cdn.rxgzs.cn/api/get-mt-web-version.php')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok.');
-            }
-            return response.text();
-        })
-        .then(version => {
-            document.getElementById("version").innerHTML = version;
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            document.getElementById("version").innerHTML = "Error occurred while fetching version.";
-        });
-}
-
-
