60c4b1bf251c013fa3f18f24c39f7e1925d3bedd
Nero978 <zhumengfeiyang@qq.com>
2023-08-09 19:37:03 +0800

    优化了导航栏的颜色

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
-let navbar = document.getElementById('nav');
+navbar = document.getElementById('nav');
+nvaLogo = document.getElementById('nav-logo');
 
 addEventListener('scroll', () => {
     if (window.scrollY > 0) {
         navbar.classList.add('navbar-scrolled');
+        nvaLogo.setAttribute('src', './img/logo.png');
+        navbar.setAttribute('data-bs-theme', 'light');
     } else {
         navbar.classList.remove('navbar-scrolled');
+        nvaLogo.setAttribute('src', './img/logo-white.png');
+        navbar.setAttribute('data-bs-theme', 'dark');
     }
 });
 
