e93500b532bc108707cd8bebd4852027250f3eef
Nero978 <zhumengfeiyang@qq.com>
2023-08-22 17:43:07 +0800

    二周年活动信息更新、活动弹窗移除、首页文案优化

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//全局按钮悬停提示
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
@@ -6,7 +7,7 @@ document.addEventListener("WeixinJSBridgeReady", function () {
     document.getElementById('banner-video').play();
 }, false);
 
-
+//复制IP
 function copyToClipboard() {
     // 获取要复制的文本内容
     let copy = document.getElementById("ServerIP");
@@ -31,15 +32,16 @@ function copyToClipboard() {
     document.body.removeChild(textarea);
 }
 
-const errorToast = document.getElementById('errorToast');
-const birthdayToast = document.getElementById('birthdayToast');
+//初始化提示框
+const errorToast = bootstrap.Toast.getOrCreateInstance(document.getElementById('errorToast'))
+const birthdayToast = bootstrap.Toast.getOrCreateInstance(document.getElementById('birthdayToast'))
 
+//初始化Get-Ping区域
 window.onload = () => {
     getPing();
-    const toastBootstrap2 = bootstrap.Toast.getOrCreateInstance(birthdayToast)
-    toastBootstrap2.show()
 }
 
+//移除Get-Ping占位符
 function displayPingInfo() {
     let pingInfo = document.getElementsByClassName("ping-info");
     for (let i = 0; i < pingInfo.length; i++) {
@@ -47,6 +49,7 @@ function displayPingInfo() {
     }
 }
 
+//Get-Ping API
 function getPing() {
     let status = document.getElementById("status");
     let people = document.getElementById("people");
@@ -56,8 +59,7 @@ function getPing() {
         .then(response => response.json()) // 将响应解析为 JSON
         .then(data => {
             if (data.errorcode == 404) {
-                const toastBootstrap = bootstrap.Toast.getOrCreateInstance(errorToast)
-                toastBootstrap.show()
+                errorToast.show()
                 status.innerText = "当前离线";
                 people.innerText = "null";
                 gameVersion.innerText = "null";
@@ -73,8 +75,7 @@ function getPing() {
         })
         .catch(error => {
             console.error('Error fetching JSON:', error);
-            const toastBootstrap = bootstrap.Toast.getOrCreateInstance(errorToast)
-            toastBootstrap.show()
+            errorToast.show()
             status.innerText = "unknown";
             people.innerText = "null";
             gameVersion.innerText = "null";
